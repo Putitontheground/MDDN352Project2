3c003582a4247bca82508fd59b78a4c9f93dde00
Guest User <Guest@wg401-h-06-07.mddn.vuw.ac.nz>
2018-06-05 11:11:29 +1200

    grid is poppin

--- js/js.js
+++ js/js.js
@@ -1,99 +1,118 @@
 var extendDeep = AFRAME.utils.extendDeep;
 
 let onload = function() {
-    var numListeners = 0;
+var marker = document.getElementById("marker");
+let boxSize = 0.1;
+let spacing = 0.02;
+for(let x = 1; x<=4;x++){
+    for(let y = 1; y<=4;y++){
+    var name = "box_"+x+"-"+y;
+    regBox(name);
+    let xpos = (x-2)*(boxSize+spacing);
+    let ypos = (y-2)*(boxSize+spacing);
+    marker.appendChild(makeBox(xpos,ypos,boxSize,name));
+}}
 
-    makeBox(0,0,0.01);
     // The mesh mixin provides common material properties for creating mesh-based primitives.
     // This makes the material component a default component and maps all the base material properties.
     var meshMixin = AFRAME.primitives.getMeshMixin();
 
-    AFRAME.registerComponent('log-cursor-listener', {
-        init: function() {
-            var isMoused = false;
-            var isMoved = false;
 
-            element = this.el;
-            element.setAttribute("id", "C-listener-" + numListeners);
+}
 
-            function move() {
-                if (!isMoved) {
-                    element.emit("move");
-                    isMoved = true;
+var regBox= function(name){
+    AFRAME.registerComponent(name, {
+        init: function() { 
+            this.mouseleaveTimeout = 250; 
+            this.isMoused = false;
+            this.isMoved = false;
+            this.name = name; 
+            this.el.parent = this;
+
+
+            this.move = function(t) {
+                if (!t.isMoved) {
+                    t.el.emit("move-"+t.name);
+                    console.log("move-"+t.name);
+                    t.isMoved = true;
                 } else {
 
-                    element.emit("return");
-                    isMoved = false;
+                    t.el.emit("return-"+t.name);
+                    t.isMoved = false;
+                    console.log("return-"+t.name);
                 }
             }
 
-            element.addEventListener('click', function(evt) {
-                if (!isMoved) {
-                    move();
+            this.el.addEventListener('click', function(evt) {
+                console.log("click");
+                if (!this.parent.isMoved) {
+                    this.parent.move(this.parent);
                 }
             });
 
-            element.addEventListener('mouseleave', function(evt) {
-                isMoused = false;
-                if (isMoved == true) {
+            this.el.addEventListener('mouseleave', function(evt) {
+                parent = this.parent;   
+
+                parent.isMoused = false;
+                if (parent.isMoved == true) {
                     setTimeout(function() {
-                        if (isMoved == true && isMoused == false) {
-                            move();
+                        if (parent.isMoved == true && parent.isMoused == false) {
+                            parent.move(parent);
                         }
-                    }, 1000);
+                    }, this.mouseleaveTimeout);
                 }
             });
-            element.addEventListener('mouseenter', function(evt) {
-                isMoused = true;
+            this.el.addEventListener('mouseenter', function(evt) {
+                this.parent.isMoused = true;
                 console.log("mouseenter");
             });
         }
     });
 }
-
-
-var generateGrid = function(boxSize,boxSpacing){
-
-}
-
-var makeBox = function(x,y,siz){
-	var z = 0;
+var makeBox = function(x,y,siz, name){
+	var z = y;
+    y=0;
 	var moveDist = siz*2.5;
-	var moveSpeed = 1200;
+	var moveSpeed = 700;
 
-	var marker = document.getElementById("marker");
-	var a_box =  document.createElement("a-box");
+    var scaleString = ""+siz+" "+siz+" "+siz;
+    var startPos = ""+x+" "+y+" "+z;
+    var endPos = ""+x+" "+y+moveDist+" "+z;
+    
+	var a_box =  document.createElement("a-entity");
+    a_box.setAttribute("id",name);
+    a_box.setAttribute("geometry","primitive:box");
+
 	var anim_move = document.createElement("a-animation");
-	var anim_return;
 
-	var scaleString = ""+siz+" "+siz+" "+siz;
-	var posString = ""+x+" "+y+" "+z;
-	console.log(typeof scaleString);
-	console.log(scaleString);
 
-	
-    //TODO: not setting these attribs for some reason    
 	a_box.setAttribute("scale", scaleString+"");
-	a_box.setAttribute("position", posString+"");
+	a_box.setAttribute("position", startPos+"");
 
 	a_box.setAttribute("class","C-listener");
-    a_box.setAttribute("log-cursor-listener","");
+    a_box.setAttribute(name,"");
 
-	anim_move.setAttribute("easing","ease-in");
+	anim_move.setAttribute("easing","ease-out-elastic");
 	anim_move.setAttribute("attribute","position");
 	anim_move.setAttribute("dur",""+moveSpeed);
 
-	anim_return = anim_move.cloneNode();
+	var anim_return = anim_move.cloneNode();
 
-	anim_move.setAttribute("begin", "move");
-	anim_return.setAttribute("begin", "return");
-	anim_move.setAttribute("class", "anim-move");
+	anim_move.setAttribute("begin", "move-"+name);
+    anim_move.setAttribute("from",startPos);
+    anim_move.setAttribute("to",endPos);
+    anim_move.setAttribute("class", "anim-move");
+
+	anim_return.setAttribute("begin", "return-"+name);
+    anim_return.setAttribute("from",endPos);
+    anim_return.setAttribute("to",startPos);
 	anim_return.setAttribute("class", "anim-return");
 
 	a_box.appendChild(anim_move);
 	a_box.appendChild(anim_return);
 
-	console.log(a_box);
+    return a_box;
+
 }
 
 
