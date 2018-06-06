252afbb4bea4e9fd3e019819fbc76d69cf0d78db
Guest User <Guest@wg401-e-01-03.mddn.vuw.ac.nz>
2018-06-06 14:31:04 +1200

    PREZ

--- js/js.js
+++ js/js.js
@@ -1,17 +1,17 @@
 var extendDeep = AFRAME.utils.extendDeep;
 
-let onload = function() {
-var marker = document.getElementById("marker");
-let boxSize = 0.1;
-let spacing = 0.02;
-for(let x = 1; x<=4;x++){
-    for(let y = 1; y<=4;y++){
-    var name = "box_"+x+"-"+y;
-    regBox(name);
-    let xpos = (x-2)*(boxSize+spacing);
-    let ypos = (y-2)*(boxSize+spacing);
-    marker.appendChild(makeBox(xpos,ypos,boxSize,name));
-}}
+window.onload = function() {
+    var marker = document.getElementById("markerboxes");
+    let boxSize = 0.5;
+    let spacing = 0.1;
+    for(let x = 1; x<=4;x++){
+        for(let y = 1; y<=3;y++){
+        var name = "box_"+x+"-"+y;
+        regBox(name);
+        let xpos = (x-2)*(boxSize+spacing);
+        let ypos = (y-2)*(boxSize+spacing);
+        marker.appendChild(makeBox(xpos,ypos,boxSize,name));
+    }}
 
     // The mesh mixin provides common material properties for creating mesh-based primitives.
     // This makes the material component a default component and maps all the base material properties.
@@ -70,50 +70,47 @@ var regBox= function(name){
     });
 }
 var makeBox = function(x,y,siz, name){
-	var z = y;
+    var z = y;
     y=0;
-	var moveDist = siz*2.5;
-	var moveSpeed = 700;
+    var moveDist = siz*2.5;
+    var moveSpeed = 700;
 
     var scaleString = ""+siz+" "+siz+" "+siz;
     var startPos = ""+x+" "+y+" "+z;
     var endPos = ""+x+" "+y+moveDist+" "+z;
     
-	var a_box =  document.createElement("a-entity");
+    var a_box =  document.createElement("a-entity");
     a_box.setAttribute("id",name);
     a_box.setAttribute("geometry","primitive:box");
 
-	var anim_move = document.createElement("a-animation");
+    var anim_move = document.createElement("a-animation");
 
 
-	a_box.setAttribute("scale", scaleString+"");
-	a_box.setAttribute("position", startPos+"");
+    a_box.setAttribute("scale", scaleString+"");
+    a_box.setAttribute("position", startPos+"");
 
-	a_box.setAttribute("class","C-listener");
+    a_box.setAttribute("class","C-listener");
     a_box.setAttribute(name,"");
 
-	anim_move.setAttribute("easing","ease-out-elastic");
-	anim_move.setAttribute("attribute","position");
-	anim_move.setAttribute("dur",""+moveSpeed);
+    anim_move.setAttribute("easing","ease-out-elastic");
+    anim_move.setAttribute("attribute","position");
+    anim_move.setAttribute("dur",""+moveSpeed);
 
-	var anim_return = anim_move.cloneNode();
+    var anim_return = anim_move.cloneNode();
 
-	anim_move.setAttribute("begin", "move-"+name);
+    anim_move.setAttribute("begin", "move-"+name);
     anim_move.setAttribute("from",startPos);
     anim_move.setAttribute("to",endPos);
     anim_move.setAttribute("class", "anim-move");
 
-	anim_return.setAttribute("begin", "return-"+name);
+    anim_return.setAttribute("begin", "return-"+name);
     anim_return.setAttribute("from",endPos);
     anim_return.setAttribute("to",startPos);
-	anim_return.setAttribute("class", "anim-return");
+    anim_return.setAttribute("class", "anim-return");
 
-	a_box.appendChild(anim_move);
-	a_box.appendChild(anim_return);
+    a_box.appendChild(anim_move);
+    a_box.appendChild(anim_return);
 
     return a_box;
 
 }
-
-
-$(document).ready(onload);
